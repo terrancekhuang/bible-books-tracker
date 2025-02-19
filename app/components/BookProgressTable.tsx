d03fc341e6267859c0f96e24c46ddbb04efd3938
Terrance Huang <notterrancehuang@gmail.com>
2025-02-18 23:43:08 -0500

    updated new queries

--- app/components/BookProgressTable.tsx
+++ app/components/BookProgressTable.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { getBooks } from "../lib/queries";
 import DataTable from "./DataTable";
+import prisma from "@/lib/db";
 
 export default async function BookProgressTable() {
-  const books = await getBooks();
+  const books = await prisma.bible_books.findMany();
+
   return (
     <div>
       <h1 className="text-xl font-bold ml-4 pt-4">Progress</h1>

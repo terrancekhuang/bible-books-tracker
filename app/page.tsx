48c1f153b16236ed65ff57d3d4bfd978e8e74103
Terrance Huang <notterrancehuang@gmail.com>
2025-02-19 21:48:44 -0500

    add auth redirect for secret pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import { auth } from "@/auth";
 import BookProgressTable from "./components/BookProgressTable";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await auth();
+  if (!session) {
+    redirect("api/auth/signin");
+  }
+
   return (
-    <div>
-      <BookProgressTable />
-    </div>
+    <>
+      <div className="flex h-screen">
+        <div className="w-1/2">
+          <BookProgressTable />
+        </div>
+
+        <div className="w-1/2">
+          <p>Well Hello There</p>
+        </div>
+      </div>
+    </>
   );
 }
--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
+const page = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect("api/auth/signin");
+  }
+
   return (
     <>
       <h1 className="text-xl font-bold p-4">Stats</h1>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;

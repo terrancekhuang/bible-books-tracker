ed0eb4e5617e408b92257fa7d2ba6342b293c92d
Terrance Huang <terrancehuang@proton.me>
2026-02-08 23:31:58 -0500

    frontend show books

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+
+interface Book {
+  name: string;
+  testament: string;
+  category: string;
+  num_chapters: number;
+}
+
 function App() {
+  const [books, set_books] = useState<Book[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/books")
+      .then((res) => res.json())
+      .then((data) => set_books(data))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold text-center">Bible Books Tracker</h1>
@@ -13,7 +31,16 @@ function App() {
               <th>Chapters</th>
             </tr>
           </thead>
-          <tbody></tbody>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={index}>
+                <td>{book.name}</td>
+                <td>{book.testament}</td>
+                <td>{book.category}</td>
+                <td>{book.num_chapters}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </>
